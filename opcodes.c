85c3468c33125c4c05b72f74d30eba15cf28a471
Samuel Ogboye <125566090+samuelogboye@users.noreply.github.com>
2023-06-22 19:51:15 +0100

    Operations added

---
+++ opcodes.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+
+/**
+ * swap - swap top two values on stack and update top/bottom
+ * @rear: rear of stack
+ */
+void swap(stack_t **front, stack_t **rear)
+{
+	stack_t *ptr = *front;
+
+	if (ptr == NULL || ptr->prev == NULL)
+		exit_true(EXIT_FAILURE, "can't swap, stack too short", *front);
+	ptr = ptr->prev;
+	(*front)->prev = ptr->prev;
+	ptr->next = (*front)->next;
+	ptr->prev = *front;
+	(*front)->next = ptr;
+	*front = ptr;
+	if ((*rear)->prev != NULL)
+		*rear = (*rear)->prev;
+}
+
+
+/**
+ * pop - pop top element of stack
+ * @front: topmost of stack
+ */
+void pop(stack_t **front)
+{
+	stack_t *ptr = *front;
+
+	if (ptr == NULL)
+		exit_true(EXIT_FAILURE, "can't pop an empty stack", *front);
+	if (ptr->prev == NULL)
+	{
+		free(*front);
+		*front = NULL;
+	}
+	else
+	{
+		ptr = ptr->prev;
+		ptr->next = NULL;
+		free(*front);
+		*front = ptr;
+	}
+}
+
+
+/**
+ * pint - print top of stack as an integer
+ * @front: topmost of stack
+ */
+void pint(stack_t **front)
+{
+	if (*front == NULL)
+		exit_true(EXIT_FAILURE, "can't pint, stack empty", *front);
+	printf("%d\n", (*front)->n);
+}
