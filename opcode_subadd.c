f2ad92ea2a9109e98a9d98ad47de849fa3f153b4
Samuel Ogboye <125566090+samuelogboye@users.noreply.github.com>
2023-06-22 19:21:49 +0100

    Create opcode_subadd.c

---
+++ opcode_subadd.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+
+/**
+ * sub - subtract top two stack elements. error if < 2 elements
+ * @top: top of stack
+ */
+void sub(stack_t **front)
+{
+	stack_t *ptr = *front;
+	long number;
+
+	if (ptr == NULL || ptr->prev == NULL)
+		exit_true(EXIT_FAILURE, "can't sub, stack too short", *front);
+	else
+	{
+		number = ptr->n;
+		ptr = ptr->prev;
+		ptr->next = NULL;
+		free(*front);
+		*front = ptr;
+		ptr->n -= number;
+	}
+}
+
+
+/**
+ * add - add top two stack elements. error if < 2 elements
+ * @top: top of stack
+ */
+void add(stack_t **front)
+{
+	stack_t *ptr = *front;
+	long number;
+
+	if (ptr == NULL || ptr->prev == NULL)
+		exit_true(EXIT_FAILURE, "can't add, stack too short", *front);
+	else
+	{
+		number = ptr->n;
+		ptr = ptr->prev;
+		ptr->next = NULL;
+		free(*front);
+		*front = ptr;
+		ptr->n += number;
+	}
+}
+
