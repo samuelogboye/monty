cd352a09ecda94a3c82eef30458bf1dd5b9c9f89
Samuel <ogboyesam@gmail.com>
2023-06-23 05:30:12 -0700

    updated stderr print out

--- monty.h
+++ monty.h
@@ -10,7 +10,7 @@
 #define QUEUEMODE 1
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack or queue
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
