0ab96edead90685cf008c4136d457c910a487e88
Samuel <ogboyesam@gmail.com>
2023-06-23 06:13:54 -0700

    ensuring betty free

--- parse.c
+++ parse.c
@@ -8,49 +8,6 @@
 static montyglob mglob;
 
 /**
- * exit_true - free things and exit the program. Print error message as needed
- * @exitcode: exit code
- * @exitstring: error string to print, if any
- * @front: topmost of stack (for freeing)
- */
-void exit_true(int exitcode, char *exitstring, stack_t *front)
-{
-	stack_t *ptr = front;
-
-	if (exitstring != NULL)
-		fprintf(stderr, "L%lu: %s\n", mglob.linenum, exitstring);
-	while (front != NULL)
-	{
-		ptr = front->prev;
-		free(front);
-		front = ptr;
-	}
-	free(mglob.buffer);
-	fclose(mglob.script);
-	exit(exitcode);
-}
-
-/**
- * is_num - checks if a string is a number
- * @s: string to check
- * Return: 1 if numeric, 0 otherwise
- */
-int is_num(char *s)
-{
-	if (*s == '-')
-	{
-		s++;
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	while (*s != 0)
-		if (*s < '0' || *s++ > '9')
-			return (0);
-	return (1);
-}
-
-/**
  * parse_eff - parser for monty script files
  * @ops: array of opcodes and pointers to functions for them
  * Return: 0 if successful
